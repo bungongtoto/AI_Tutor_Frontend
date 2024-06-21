01ea770b788750ba1afe4f7d84416fe54decb901
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-06-21 16:34:09 +0100

    added the home footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react";
+import { FaEnvelope } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className='home_footer'>
-        <div className='home_footer_container'>
-          <h1>Footer</h1>
+    <footer className="home_footer">
+      <div className="home_footer_container">
+        <h1>Contact Us</h1>
+        <div className="contact-tile">
+          <FaEnvelope className="contact-icon" />
+          <span>info.contact@aitutor.com</span>
         </div>
-    </footer>
-  )
-}
+        <div className="contact-tile">
+          <FaPhoneAlt className="contact-icon" />
+          <span>+237 6 xx xx xx xx</span>
+        </div>
+        <hr />
+        <p>&copy; 2024 AI-Tutor. All rights reserved.</p>
 
-export default Footer
+        <hr />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
